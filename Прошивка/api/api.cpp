55d8b6478f0fa6bed67dfc77fac2f49696caea12
Unknown <vasilek-1997@tut.by>
2017-10-23 01:09:23 +0300

    Добавлено API взаимодействия с роботом

---
+++ Прошивка/api/api.cpp
@@ -0,0 +1,84 @@
+#include "api.h"
+
+void API::setupServo(int pin)
+{
+    servo.attach(pin);
+}
+
+void API::setupDrives(int IN1pin, int IN2pin, int IN3pin, int IN4pin)
+{
+    driveIN1pin = IN1pin;
+    driveIN2pin = IN2pin;
+    driveIN3pin = IN3pin;
+    driveIN4pin = IN4pin;
+    pinMode(IN1pin, OUTPUT);
+    pinMode(IN2pin, OUTPUT);
+    pinMode(IN3pin, OUTPUT);
+    pinMode(IN4pin, OUTPUT);
+}
+
+void API::setupSensors(int firstIRpin, int secondIRpin, int thirdIRpin, long model)
+{
+    firstIR = new SharpIR(firstIRpin, model);  
+    secondIR = new SharpIR(secondIRpin, model);  
+    thirdIR = new SharpIR(thirdIRpin, model);  
+}
+
+void API::turnLeft()
+{
+    servo.write(mainAngle - rotateAngle);
+}
+
+void API::turnRight()
+{
+    servo.write(mainAngle + rotateAngle);
+}
+
+void API::turnStraight()
+{
+    servo.write(mainAngle);
+}
+
+void API::goStraight()
+{
+    digitalWrite (driveIN1pin, LOW);
+    digitalWrite (driveIN2pin, HIGH);
+
+    digitalWrite (driveIN3pin, LOW);
+    digitalWrite (driveIN4pin, HIGH);
+}
+
+void API::goBack()
+{
+    digitalWrite (driveIN1pin, HIGH);
+    digitalWrite (driveIN2pin, LOW);
+
+    digitalWrite (driveIN3pin, HIGH);
+    digitalWrite (driveIN4pin, LOW);
+}
+
+void API::stopDrives()
+{
+    digitalWrite (driveIN1pin, LOW);
+    digitalWrite (driveIN2pin, LOW);
+
+    digitalWrite (driveIN3pin, LOW);
+    digitalWrite (driveIN4pin, LOW);
+}
+
+int API::firstIRDistance()
+{
+    return firstIR->distance();
+}
+    
+int API::secondIRDistance()
+{
+    return secondIR->distance();
+}
+
+int API::thirdIRDistance()
+{
+    return thirdIR->distance();
+}
+
+
